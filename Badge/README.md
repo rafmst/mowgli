07601f020eafec788e2d05dabdfdb55f31b6de3a
Rafael Morais <rafaelcanical@gmail.com>
2020-06-29 21:11:49 +0200

    Readme & Better props

--- Badge/README.md
+++ Badge/README.md
@@ -9,9 +9,7 @@ Badge component for React Native apps.
     <a href="https://github.com/rafmst/nau/blob/master/LICENSE"><img src="https://img.shields.io/github/license/rafmst/mowgli"></a>
 </p>
 
-## Preview
-
-<img src="https://user-images.githubusercontent.com/924985/85956431-2b36f680-b986-11ea-8ebe-2b34994d13b1.png" />
+<img src="https://user-images.githubusercontent.com/924985/86044217-1a4fb900-ba4a-11ea-8aa6-b48571d6c3f7.png" />
 
 ## Instalation
 
@@ -26,25 +24,59 @@ yarn add @mowgli/badge
 ## Usage
 
 ```jsx
-<Badge
-    text="Sent"
-    success
-    big
->
+import Badge from '@mowgli/badge'
+
+render() {
+    <Badge text="Sent" accent="primary" size="big" full />
+}
 ```
 
 ## Props
 
-| Name      | Type      | Description                                       |
-| --------- | --------- | ------------------------------------------------- |
-| `text`    | `string`  | The text to be displayed                          |
-| `primary` | `boolean` | Gives a blue accent color                         |
-| `success` | `boolean` | Gives a green accent color                        |
-| `info`    | `boolean` | Gives a light blue accent color                   |
-| `warning` | `boolean` | Gives a yellow accent color                       |
-| `danger`  | `boolean` | Gives a red accent color                          |
-| `color`   | `string`  | Custom accent color                               |
-| `small`   | `boolean` | Smaller badge                                     |
-| `medium`  | `boolean` | Normal size badge                                 |
-| `big`     | `boolean` | Bigger badge                                      |
-| `full`    | `boolean` | If should display with full with, default `false` |
+- [`text`](#text)
+- [`color`](#color)
+- [`accent`](#accent)
+- [`size`](#size)
+- [`fullWidth`](#fullwidth)
+
+## Reference
+
+#### `text`
+
+Contains the text to display.
+
+| Type   | Default |
+| ------ | ------- |
+| string | _empty_ |
+
+#### `color`
+
+If you don't like any of the accent colors available you can set a custom background color with the property.
+
+| Type   | Default |
+| ------ | ------- |
+| string | _empty_ |
+
+#### `accent`
+
+Determines the accent/background color.
+
+| Type                                             | Default |
+| ------------------------------------------------ | ------- |
+| `primary`, `success`, `info`, `warning`, `error` | primary |
+
+#### `size`
+
+Determines the size of the badge.
+
+| Type                       | Default |
+| -------------------------- | ------- |
+| `small`, `medium`, `large` | medium  |
+
+#### `fullWidth`
+
+Determines if the badge should occupy all the available space.
+
+| Type    | Default |
+| ------- | ------- |
+| boolean | false   |
--- Badge/src/index.tsx
+++ Badge/src/index.tsx
@@ -1,68 +1,81 @@
-import React from "react";
-import { View, Text, Platform } from "react-native";
+import React from 'react'
+import { View, Text } from 'react-native'
 
+// Props is the interface of this component props
 interface Props {
-    text: string;
-    primary?: boolean;
-    success?: boolean;
-    info?: boolean;
-    warning?: boolean;
-    danger?: boolean;
-    color?: string;
-    small?: boolean;
-    medium?: boolean;
-    big?: boolean;
-    full?: boolean;
+  text: string
+  accent?: string
+  color?: string
+  size?: string
+  fullWidth?: boolean
+}
+
+// StringMap is a simple interface that maps string objects
+interface StringMap {
+  [key: string]: string
+}
+
+// NumberMap is a simple interface that maps number objects
+interface NumberMap {
+  [key: string]: number
+}
+
+// colors list for accents
+const colors: StringMap = {
+  primary: '#3366FF',
+  success: '#97CC06',
+  info: '#00AEFF',
+  warning: '#FFC311',
+  danger: '#FF472B'
+}
+
+// sizes list, for fontSize option
+const sizes: NumberMap = {
+  small: 12,
+  medium: 14,
+  big: 16
 }
 
 class Badge extends React.Component<Props> {
-    render() {
-        const { text, primary, success, info, warning, danger, color, small, medium, big, full } = this.props;
+  render() {
+    const { text, accent, color, size, fullWidth } = this.props
 
-        const borderRadius: number = Platform.OS === "ios" ? 120 : 4;
-
-        let backgroundColor: string = "#3366FF";
-        if (primary) {
-            backgroundColor = "#3366FF";
-        } else if (success) {
-            backgroundColor = "#97CC06";
-        } else if (info) {
-            backgroundColor = "#00AEFF";
-        } else if (warning) {
-            backgroundColor = "#FFC311";
-        } else if (danger) {
-            backgroundColor = "#FF472B";
-        } else if (color) {
-            backgroundColor = color;
-        }
-
-        let fontSize: number = 14;
-        if (small) {
-            fontSize = 12;
-        } else if (medium) {
-            fontSize = 14;
-        } else if (big) {
-            fontSize = 16;
-        }
-
-        return (
-            <View style={full ? {} : { flexDirection: "row" }}>
-                <View
-                    style={[
-                        {
-                            paddingVertical: 4,
-                            paddingHorizontal: 10,
-                            borderRadius,
-                            backgroundColor,
-                        },
-                        full ? { alignItems: "center" } : {},
-                    ]}
-                >
-                    <Text style={{ fontSize, color: "white" }}>{text}</Text>
-                </View>
-            </View>
-        );
+    // Define the backgroundColor property
+    let backgroundColor: string
+    if (accent && typeof colors[accent] !== 'undefined') {
+      backgroundColor = colors[accent]
+    } else if (color) {
+      backgroundColor = color
+    } else {
+      backgroundColor = colors.primary
     }
+
+    // Define the fontSize property
+    let fontSize: number
+    if (size && typeof sizes[size] !== 'undefined') {
+      fontSize = sizes[size]
+    } else {
+      fontSize = sizes.medium
+    }
+
+    return (
+      <View style={fullWidth ? {} : { flexDirection: 'row' }}>
+        <View
+          style={[
+            {
+              paddingVertical: 3,
+              paddingHorizontal: 8,
+              borderRadius: 120,
+              backgroundColor
+            },
+            fullWidth ? { alignItems: 'center' } : {}
+          ]}
+        >
+          <Text style={{ fontSize, color: 'white' }}>{text}</Text>
+        </View>
+      </View>
+    )
+  }
 }
 
-export default Badge;
+export default Badge
